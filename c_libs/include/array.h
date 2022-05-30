b146b5f96c34012912a1a60f2e7c66560523529e
Jon Webb <webbja123@gmail.com>
2022-05-30 07:59:38 -0600

    Added append_array functions with unit tests and doc strings

--- c_libs/include/array.h
+++ c_libs/include/array.h
@@ -96,6 +96,146 @@ int pop_double_array(double *array, int index, int size);
  * @endcode
  */
 int pop_char_array(char *array, int index, int size);
+// --------------------------------------------------------------------------------
+
+/**
+ * This function will append an existing array with a sclar value or another
+ * array.  WARNING: The size the data within the parent_array combined with
+ * the data in the child_array cannot exceed the total allocation of memory
+ * for the parent_array
+ *
+ * @param parent_array The integer array to which more data will be added
+ * @param len The number of allocated indices in the parent_array, not to be
+ *            confused with the total number of allocated indices
+ * @param child_array A integer scalar or integer array of data to be added
+ *                    to the parent_array
+ * @param count The total number of indices to be added to the parent_array
+ *              from the child_array
+ *
+ * @code
+ * int a[9] = {1, 2, 3};
+ * int b[6] = [4, 5, 6, 7, 8, 9];
+ * append_int_array(a, 3, b, 6);
+ * for (int i = 0; i < 9; i++) {
+ *     printf("%d\n"i, a[i]);
+ * }
+ * // >> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ *
+ * int a[9] = {1, 2, 3, 4, 5, 6, 7, 8};
+ * int b = 9;
+ * append_int_array(a, 8, &b, 1);
+ * for (int i = 0; i < 9; i++) {
+ * printf("%d\n", a[i]);
+ * }
+ * // >> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ * @endcode
+ */
+void append_int_array(int *parent_array, int len, int *child_array, size_t count);
+// --------------------------------------------------------------------------------
+
+/**
+ * This function will append an existing array with a sclar value or another
+ * array.  WARNING: The size the data within the parent_array combined with
+ * the data in the child_array cannot exceed the total allocation of memory
+ * for the parent_array
+ *
+ * @param parent_array The float array to which more data will be added
+ * @param len The number of allocated indices in the parent_array, not to be
+ *            confused with the total number of allocated indices
+ * @param child_array A float scalar or integer array of data to be added
+ *                    to the parent_array
+ * @param count The total number of indices to be added to the parent_array
+ *              from the child_array
+ *
+ * @code
+ * float a[9] = {1.1, 2.2, 3.3};
+ * float b[6] = [4.4, 5.5, 6.6, 7.7, 8.8, 9.9];
+ * append_float_array(a, 3, b, 6);
+ * for (int i = 0; i < 9; i++) {
+ *     printf("%f\n"i, a[i]);
+ * }
+ * // >> [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9]
+ *
+ * float a[9] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8};
+ * float b = 9.9;
+ * append_float_array(a, 8, &b 1);
+ * for (int i = 0; i < 9; i++) {
+ *     printf("%f\n", a[i]);
+ * }
+ * // >> [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9]
+ * @endcode
+ */
+void append_float_array(float *parent_array, int len, float *child_array, size_t count);
+// --------------------------------------------------------------------------------
+
+/**
+ * This function will append an existing array with a sclar value or another
+ * array.  WARNING: The size the data within the parent_array combined with
+ * the data in the child_array cannot exceed the total allocation of memory
+ * for the parent_array
+ *
+ * @param parent_array The double array to which more data will be added
+ * @param len The number of allocated indices in the parent_array, not to be
+ *            confused with the total number of allocated indices
+ * @param child_array A double scalar or integer array of data to be added
+ *                    to the parent_array
+ * @param count The total number of indices to be added to the parent_array
+ *              from the child_array
+ *
+ * @code
+ * double a[9] = {1.1, 2.2, 3.3};
+ * double b[6] = [4.4, 5.5, 6.6, 7.7, 8.8, 9.9];
+ * append_double_array(a, 3, b, 6);
+ * for (int i = 0; i < 9; i++) {
+ *     printf("%lf\n"i, a[i]);
+ * }
+ * // >> [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9]
+ *
+ * double a[9] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8};
+ * double b = 9.9;
+ * append_double_array(a, 8, &b 1);
+ * for (int i = 0; i < 9; i++) {
+ *     printf("%lf\n", a[i]);
+ * }
+ * // >> [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9]
+ * @endcode
+ */
+void append_double_array(double *parent_array, int len, double *child_array, size_t count);
+// --------------------------------------------------------------------------------
+
+/**
+ * This function will append an existing array with a sclar value or another
+ * array.  WARNING: The size the data within the parent_array combined with
+ * the data in the child_array cannot exceed the total allocation of memory
+ * for the parent_array
+ *
+ * @param parent_array The char array to which more data will be added
+ * @param len The number of allocated indices in the parent_array, not to be
+ *            confused with the total number of allocated indices
+ * @param child_array A char scalar or integer array of data to be added
+ *                    to the parent_array
+ * @param count The total number of indices to be added to the parent_array
+ *              from the child_array
+ *
+ * @code
+ * char a[10] = "abc";
+ * char b[7] = "defghi"
+ * append_double_array(a, 3, b, 6);
+ * for (int i = 0; i < 9; i++) {
+ *     printf("%lf\n"i, a[i]);
+ * }
+ * // >> ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+ *
+ * char a[10] = "abcdefgh";
+ * char b = 'i'
+ * append_double_array(a, 9, &b 1);
+ * for (int i = 0; i < 9; i++) {
+ *     printf("%lf\n", a[i]);
+ * }
+ * // >> ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+ * @endcode
+ */
+void append_char_array(char *parent_array, int len, char *child_array, size_t count);
 #endif /* array_H */
 // ================================================================================
 // ================================================================================
--- c_libs/test/test_array.cpp
+++ c_libs/test/test_array.cpp
@@ -86,5 +86,100 @@ TEST(test_pop_array, pop_char) {
 	EXPECT_EQ(a[7], 'i');
 }
 // ================================================================================
+
+/* This function will test the test append_int_array function to ensure it
+ * properly appends a parent array with a child array
+ */
+TEST(test_append_array, append_int_one) {
+	int a[9] = {1, 2, 3};
+	int b[6] = {4, 5, 6, 7, 8, 9};
+	append_int_array(a, 3, b, 6);
+	EXPECT_EQ(a[0], 1);
+	EXPECT_EQ(a[1], 2);
+	EXPECT_EQ(a[2], 3);
+	EXPECT_EQ(a[3], 4);
+	EXPECT_EQ(a[4], 5);
+	EXPECT_EQ(a[5], 6);
+	EXPECT_EQ(a[6], 7);
+	EXPECT_EQ(a[7], 8);
+	EXPECT_EQ(a[8], 9);
+}
+// --------------------------------------------------------------------------------
+
+/* This function will test the test append_int_array function to ensure it
+ * properly appends a parent array with a child scalar
+ */
+TEST(test_append_array, append_int_two) {
+	int a[9] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int b = 9;
+	append_int_array(a, 8, &b, 1);
+	EXPECT_EQ(a[0], 1);
+	EXPECT_EQ(a[1], 2);
+	EXPECT_EQ(a[2], 3);
+	EXPECT_EQ(a[3], 4);
+	EXPECT_EQ(a[4], 5);
+	EXPECT_EQ(a[5], 6);
+	EXPECT_EQ(a[6], 7);
+	EXPECT_EQ(a[7], 8);
+	EXPECT_EQ(a[8], 9);
+}
+// --------------------------------------------------------------------------------
+
+/* This function will test the test append_float_array function to ensure it
+ * properly appends a parent array with a child array
+ */
+TEST(test_append_array, append_float) {
+	float a[9] = {1.1, 2.2, 3.3};
+	float b[6] = {4.4, 5.5, 6.6, 7.7, 8.8, 9.9};
+	append_float_array(a, 3, b, 6);
+	EXPECT_FLOAT_EQ(a[0], 1.1f);
+	EXPECT_FLOAT_EQ(a[1], 2.2f);
+	EXPECT_FLOAT_EQ(a[2], 3.3f);
+	EXPECT_FLOAT_EQ(a[3], 4.4f);
+	EXPECT_FLOAT_EQ(a[4], 5.5f);
+	EXPECT_FLOAT_EQ(a[5], 6.6f);
+	EXPECT_FLOAT_EQ(a[6], 7.7f);
+	EXPECT_FLOAT_EQ(a[7], 8.8f);
+	EXPECT_FLOAT_EQ(a[8], 9.9f);
+}
+// --------------------------------------------------------------------------------
+
+/* This function will test the test append_double_array function to ensure it
+ * properly appends a parent array with a child array
+ */
+TEST(test_append_array, append_double) {
+	double a[9] = {1.1, 2.2, 3.3};
+	double b[6] = {4.4, 5.5, 6.6, 7.7, 8.8, 9.9};
+	append_double_array(a, 3, b, 6);
+	EXPECT_DOUBLE_EQ(a[0], 1.1);
+	EXPECT_DOUBLE_EQ(a[1], 2.2);
+	EXPECT_DOUBLE_EQ(a[2], 3.3);
+	EXPECT_DOUBLE_EQ(a[3], 4.4);
+	EXPECT_DOUBLE_EQ(a[4], 5.5);
+	EXPECT_DOUBLE_EQ(a[5], 6.6);
+	EXPECT_DOUBLE_EQ(a[6], 7.7);
+	EXPECT_DOUBLE_EQ(a[7], 8.8);
+	EXPECT_DOUBLE_EQ(a[8], 9.9);
+}
+// --------------------------------------------------------------------------------
+
+/* This function will test the test append_char_array function to ensure it
+ * properly appends a parent array with a child array
+ */
+TEST(test_append_array, append_char) {
+	char a[10] = "abcdefgh";
+	char b = 'i';
+	append_char_array(a, 8, &b, 1);
+	EXPECT_EQ(a[0], 'a');
+	EXPECT_EQ(a[1], 'b');
+	EXPECT_EQ(a[2], 'c');
+	EXPECT_EQ(a[3], 'd');
+	EXPECT_EQ(a[4], 'e');
+	EXPECT_EQ(a[5], 'f');
+	EXPECT_EQ(a[6], 'g');
+	EXPECT_EQ(a[7], 'h');
+	EXPECT_EQ(a[8], 'i');
+}
+// ================================================================================
 // ================================================================================
 // eof
